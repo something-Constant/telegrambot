a7af19019514fdbd1cba857c9c67e0c59a5ad6fa
Rolex <mohamadmeymandi82@gmail.com>
2026-05-14 02:31:41 +0330

    run on leapcell 2:32

--- Bot/main.py
+++ Bot/main.py
@@ -1,4 +1,53 @@
+import asyncio
+import logging
+import os
+
+import logging
+import asyncio
+import threading
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import Update
+from aiogram.filters import Command
+
+import flask, json
+from flask import request, Flask
+from aiohttp import web
+from aiogram import Bot, Dispatcher, Router
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+
+
+from aiogram.client.session.aiohttp import AiohttpSession
+
+from aiogram import Bot, Dispatcher, F, Router, loggers, types
+from aiogram.filters import (
+    IS_MEMBER,
+    IS_NOT_MEMBER,
+    ChatMemberUpdatedFilter,
+)
+from aiogram.fsm.context import FSMContext
+from aiogram.utils.keyboard import (
+    InlineKeyboardBuilder,
+    ReplyKeyboardBuilder,
+    KeyboardBuilder,
+)
+
+from aiogram.methods import send_message
+
+from aiogram.filters import Command
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
+
+from aiogram.fsm.state import StatesGroup, State
+
 from contextlib import asynccontextmanager
+import os
+import sys
 
 from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher
@@ -8,47 +57,97 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.exceptions import TelegramRetryAfter
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+### Local Module
+# from Bot.config import TOKEN, generate_keyboard
+from Bot.Handlers.Keyboards import keyboard_router
+from Bot.Handlers.Global_Commands import globalcommand_router
+
+# from DataBase.database import globalcommand_router
+from Bot.Database.database import create_tables
+
 # =========================
 # CONFIG
 # =========================
 
-TOKEN = "7058704613:AAEuFunpv6m3jogUOXFR-0rEBrYmuQkxQh0"
-
+TOKEN = os.getenv("TOKEN")
 
 WEBHOOK_PATH = f"/webhook/{TOKEN}"
 WEBHOOK_URL = f"https://mohamadmeymandi.leapcell.app{WEBHOOK_PATH}"
-
 HOST = "0.0.0.0"
 PORT = 8080
 
-# =========================
-# BOT
-# =========================
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+
+### routing
+Root = Dispatcher()
 router = Router()
+Root.include_router(router)
+Root.include_router(globalcommand_router)
+Root.include_router(keyboard_router)
 
 
-# =========================
-# HANDLERS
-# =========================
+### makeing the inline base menus
+inline_builder = InlineKeyboardBuilder()
+inline_keys = {"⬅ Back": "Back", "▶ Start": "Start", "❌ Cancel": "Cancel"}
 
-@router.message(CommandStart())
-async def start_handler(message: Message):
-    await message.answer("Bot is running 🚀")
+for item in inline_keys:
+    inline_builder.button(text=item, callback_data=f"{inline_keys[item]}")
+inline_builder.adjust(2, 1)
+inline_keyboard = inline_builder.as_markup(resize_keyboard=True)
+
+# @router.chat_member(ChatMemberUpdatedFilter(IS_MEMBER >> IS_NOT_MEMBER))
+# async def on_user_leave(event: types.ChatMemberUpdated):
+#     pass
 
 
-dp.include_router(router)
+# @router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+# async def on_user_join(event: types.ChatMemberUpdated):
+#     pass
+
+
+@router.error()
+async def error_handler(event: types.ErrorEvent):
+    loggers.critical("Critical error caused by %s", event.exception, exc_info=True)
+
+
+@router.callback_query(F.data == "cancel")
+async def back(call: types.CallbackQuery, state: FSMContext):
+    await call.message.delete()
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        types.BotCommand(command="start", description="Start the bot / Restart"),
+        types.BotCommand(command="help", description="Help & instructions"),
+        types.BotCommand(command="menu", description="Show main menu"),
+        types.BotCommand(command="setting", description="Bot settings ⚙️"),
+    ]
+
+    await bot.set_chat_menu_button(menu_button=types.MenuButtonCommands(text="Menu"))
+
+    success = await bot.set_my_commands(
+        commands=commands,
+        scope=types.BotCommandScopeDefault(),  # default = for all private chats
+        # language_code="ru"             # optional: for specific language
+    )
+
+    return success  # True on success
 
 
 # =========================
 # FASTAPI LIFESPAN
 # =========================
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Starting bot...")
+
+    await create_tables()
+    await set_commands(bot)
 
     try:
         webhook_info = await bot.get_webhook_info()
@@ -100,7 +199,7 @@ async def telegram_webhook(request: Request):
 
     update = Update.model_validate(data)
 
-    await dp.feed_update(bot, update)
+    await Root.feed_update(bot, update)
 
     return {"ok": True}
 
